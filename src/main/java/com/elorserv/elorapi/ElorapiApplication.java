e54b979f6f24cb1e5f963b02203020045622db72
Julen-Elorrieta <julen.fernandezlo@elorrieta-errekamari.com>
2026-01-15 10:42:26 +0100

    first commit springboot

---
+++ src/main/java/com/elorserv/elorapi/ElorapiApplication.java
@@ -0,0 +1,13 @@
+package com.elorserv.elorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElorapiApplication.class, args);
+	}
+
+}
